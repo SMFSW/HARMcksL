1a30e6c2f3a5ca5faa14a09de4de99bf1b90f614
Travis CI <travis@travis-ci.org>
2018-05-05 16:03:19 +0000

    Deploy code docs to GitHub Pages Travis build: 2
    Commit: 3dd529156726b5482ee39b03c725298b772c2b2e

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -3,7 +3,7 @@ var searchData=
   ['random_5fget',['random_Get',['../random__utils_8c.html#ac1b427e79b4389d012118de4a1679af2',1,'random_Get(uint32_t start):&#160;random_utils.c'],['../random__utils_8h.html#ac1b427e79b4389d012118de4a1679af2',1,'random_Get(uint32_t start):&#160;random_utils.c']]],
   ['random_5futils_2ec',['random_utils.c',['../random__utils_8c.html',1,'']]],
   ['random_5futils_2eh',['random_utils.h',['../random__utils_8h.html',1,'']]],
-  ['read_5fgpio',['read_GPIO',['../_g_p_i_o__ex_8h.html#ada1f285b9c79da6e3791cfd86b18154c',1,'GPIO_ex.h']]],
+  ['read_5fgpio',['read_GPIO',['../_g_p_i_o__ex_8h.html#ab572d291bef748e74464f1ba674103c7',1,'GPIO_ex.h']]],
   ['repeat',['repeat',['../struct_g_p_i_o__in.html#ac19ea34f74541c48987e9313aca2193c',1,'GPIO_in']]],
   ['rtc_5fex_2ec',['RTC_ex.c',['../_r_t_c__ex_8c.html',1,'']]],
   ['rtc_5fex_2eh',['RTC_ex.h',['../_r_t_c__ex_8h.html',1,'']]],
--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['write_5fgpio',['write_GPIO',['../_g_p_i_o__ex_8h.html#a687ee7d1084530991a50b8cbb0d0efba',1,'GPIO_ex.h']]]
+  ['write_5fccr',['write_CCR',['../_p_w_m_8c.html#a443b4a0d6058d98a8f5ec0a8526ddc23',1,'PWM.c']]],
+  ['write_5fgpio',['write_GPIO',['../_g_p_i_o__ex_8h.html#ab6cf23b65abce5be6e5fa7fc9f03acf4',1,'GPIO_ex.h']]]
 ];
