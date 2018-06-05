cc6925da8f4ba94fbb8532be1da5f79ee4daf472
SMFSW <xgarmanboziax@gmail.com>
2018-06-05 18:35:39 +0200

    v1.4: added CRC_stm32 module and using it in flash_crc.c

---
+++ CRC_stm32.c
@@ -0,0 +1,58 @@
+/*!\file CRC_stm32.c
+** \author SMFSW
+** \copyright MIT (c) 2017-2018, SMFSW
+** \brief Function dealing with STM32 CRC peripheral
+*/
+/****************************************************************/
+#include <stdlib.h>
+
+#include "sarmfsw.h"
+#include "CRC_stm32.h"
+/****************************************************************/
+
+
+FctERR crc_compute(uint32_t * crc, const eCRCFeedSize feed_size, const uint32_t start_addr, const uint32_t size)
+{
+	div_t			len;
+	uint8_t *		dat8 = NULL;
+	uint16_t *		dat16 = NULL;
+	uint32_t *		dat32 = NULL;
+
+	if (feed_size == CRC_Feed_BYTE)			{ dat8 = (uint8_t *) start_addr; }
+	else if (feed_size == CRC_Feed_WORD)	{ dat16 = (uint16_t *) start_addr; }
+	else if  (feed_size == CRC_Feed_DWORD)	{ dat32 = (uint32_t *) start_addr; }
+	else									{ return ERROR_VALUE; }
+
+	len = div(size, feed_size);
+	if (len.rem)	{ return ERROR_COMMON; }
+
+	// Enable CRC
+	__HAL_RCC_CRC_CLK_ENABLE();
+
+	CRC->CR |= CRC_CR_RESET;
+
+	while (len.quot--)
+	{
+		if (feed_size == CRC_Feed_DWORD)		{ CRC->DR = *dat32++; }
+		else if (feed_size == CRC_Feed_WORD)	{ CRC->DR = *dat16++; }
+		else									{ CRC->DR = *dat8++; }
+	}
+
+	*crc = CRC->DR;
+
+	// Disable CRC
+	__HAL_RCC_CRC_CLK_DISABLE();
+
+	return ERROR_OK;
+}
+
+
+FctERR crc_check(const uint32_t crc_ref, const eCRCFeedSize feed_size, const uint32_t start_addr, const uint32_t size)
+{
+	uint32_t	crc = 0;
+
+	FctERR		err = crc_compute(&crc, feed_size, start_addr, size);
+	if (err)	{ return err; }
+
+	return (crc_ref == crc) ? ERROR_OK : ERROR_CRC;
+}
---
+++ CRC_stm32.h
@@ -0,0 +1,51 @@
+/*!\file CRC_stm32.h
+** \author SMFSW
+** \copyright MIT (c) 2017-2018, SMFSW
+** \brief Function dealing with STM32 CRC peripheral
+*/
+/****************************************************************/
+#ifndef __CRCSTM32_H__
+	#define __CRCSTM32_H__
+
+#include "sarmfsw.h"
+/****************************************************************/
+
+
+/*!\enum eCRCFeedSize
+** \brief CRC feed size
+**/
+typedef enum eCRCFeedSize {
+	CRC_Feed_BYTE = 1,		//!< CRC Feed is 8b size
+	CRC_Feed_WORD = 2,		//!< CRC Feed is 16b size
+	CRC_Feed_DWORD = 4,		//!< CRC Feed is 32b size
+} eCRCFeedSize;
+
+
+/*!\brief CRC computation
+** \param[in,out] crc - Pointer to address to store CRC
+** \param[in] feed_size - CRC peripheral feed size
+** \param[in] start_addr - Start address of block to compute CRC
+** \param[in] size - Size of block to compute CRC (in Bytes)
+** \retval ERROR_OK - CRC computed
+** \retval ERROR_VALUE - feed_size is not a value of enum eCRCFeedSize
+** \retval ERROR_COMMON - size is not int division with feed_size
+**/
+FctERR crc_compute(uint32_t * crc, const eCRCFeedSize feed_size, const uint32_t start_addr, const uint32_t size);
+
+
+/*!\brief CRC check
+** \param[in] crc_ref - CRC to check against
+** \param[in] feed_size - CRC peripheral feed size
+** \param[in] start_addr - Start address of block to compute CRC
+** \param[in] size - Size of block to compute CRC (in Bytes)
+** \retval ERROR_OK - CRC consistent
+** \retval ERROR_CRC - CRC inconsistent
+** \retval ERROR_VALUE - feed_size is not a value of enum eCRCFeedSize
+** \retval ERROR_COMMON - size is not int division with feed_size
+**/
+FctERR crc_check(const uint32_t crc_ref, const eCRCFeedSize feed_size, const uint32_t start_addr, const uint32_t size);
+
+
+/****************************************************************/
+#endif
+/****************************************************************/
