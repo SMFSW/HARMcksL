c90c4c8d7de7114524608d4546de8384eaac4e39
Travis CI <travis@travis-ci.org>
2018-10-11 19:22:50 +0000

    Deploy code docs to GitHub Pages Travis build: 9
    Commit: 09e725bbef8d9d33f07da0341be8fb6c83a79c80

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -3,5 +3,5 @@ var searchData=
   ['filt',['filt',['../struct_g_p_i_o__in.html#a000a4f2b3f81b9cc65e6a4abc6509e7c',1,'GPIO_in']]],
   ['flash_5fcheck_2ec',['flash_check.c',['../flash__check_8c.html',1,'']]],
   ['flash_5fcheck_2eh',['flash_check.h',['../flash__check_8h.html',1,'']]],
-  ['flash_5fcrc_5fcheck',['flash_crc_check',['../flash__check_8c.html#a879767630ecc6fea8bcc33417c959fd3',1,'flash_crc_check(const uint32_t flash_size):&#160;flash_check.c'],['../flash__check_8h.html#a879767630ecc6fea8bcc33417c959fd3',1,'flash_crc_check(const uint32_t flash_size):&#160;flash_check.c']]]
+  ['flash_5fcrc_5fcheck',['flash_crc_check',['../flash__check_8c.html#aaf11be85677bd5d169e06171fe723188',1,'flash_crc_check(void):&#160;flash_check.c'],['../flash__check_8h.html#aaf11be85677bd5d169e06171fe723188',1,'flash_crc_check(void):&#160;flash_check.c']]]
 ];
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['flash_5fcrc_5fcheck',['flash_crc_check',['../flash__check_8c.html#a879767630ecc6fea8bcc33417c959fd3',1,'flash_crc_check(const uint32_t flash_size):&#160;flash_check.c'],['../flash__check_8h.html#a879767630ecc6fea8bcc33417c959fd3',1,'flash_crc_check(const uint32_t flash_size):&#160;flash_check.c']]]
+  ['flash_5fcrc_5fcheck',['flash_crc_check',['../flash__check_8c.html#aaf11be85677bd5d169e06171fe723188',1,'flash_crc_check(void):&#160;flash_check.c'],['../flash__check_8h.html#aaf11be85677bd5d169e06171fe723188',1,'flash_crc_check(void):&#160;flash_check.c']]]
 ];
