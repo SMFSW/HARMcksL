80ecbb8be370ac9b4e521498306883ba62823278
Travis CI <travis@travis-ci.org>
2018-12-27 22:47:50 +0000

    Deploy code docs to GitHub Pages Travis build: 15
    Commit: f4cf36bb7296fdb1e8ca4237fc72d57df7e08d11

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -2,6 +2,11 @@ var searchData=
 [
   ['uart_5fterm_2ec',['UART_term.c',['../_u_a_r_t__term_8c.html',1,'']]],
   ['uart_5fterm_2eh',['UART_term.h',['../_u_a_r_t__term_8h.html',1,'']]],
+  ['uart_5fterm_5fflush_5frxbuf',['UART_Term_Flush_RxBuf',['../_u_a_r_t__term_8c.html#adc5914004a2824b96bdfc60270661265',1,'UART_Term_Flush_RxBuf(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#adc5914004a2824b96bdfc60270661265',1,'UART_Term_Flush_RxBuf(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
+  ['uart_5fterm_5flaunch_5fit_5frx',['UART_Term_Launch_It_Rx',['../_u_a_r_t__term_8c.html#a5fec4b49c0e0c2ff278b62af62cba68e',1,'UART_Term_Launch_It_Rx(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a5fec4b49c0e0c2ff278b62af62cba68e',1,'UART_Term_Launch_It_Rx(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
+  ['uart_5fterm_5fmessage_5fhandler',['UART_Term_Message_Handler',['../_u_a_r_t__term_8c.html#a04c0be8da4882338152488d93e2b20b3',1,'UART_Term_Message_Handler(const char *msg, const uint8_t len):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a04c0be8da4882338152488d93e2b20b3',1,'UART_Term_Message_Handler(const char *msg, const uint8_t len):&#160;UART_term.c']]],
   ['uart_5fterm_5frxcpltcallback',['UART_Term_RxCpltCallback',['../_u_a_r_t__term_8c.html#ae656c9b479c9951a030d8652a6ce3174',1,'UART_Term_RxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#ae656c9b479c9951a030d8652a6ce3174',1,'UART_Term_RxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
-  ['uart_5fterm_5ftxcpltcallback',['UART_Term_TxCpltCallback',['../_u_a_r_t__term_8c.html#a09b5de01594f460479d09051e1171dea',1,'UART_Term_TxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a09b5de01594f460479d09051e1171dea',1,'UART_Term_TxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c']]]
+  ['uart_5fterm_5fsend',['UART_Term_Send',['../_u_a_r_t__term_8h.html#ace5e5783a988679d768b16bfe5bc87c0',1,'UART_term.h']]],
+  ['uart_5fterm_5ftxcpltcallback',['UART_Term_TxCpltCallback',['../_u_a_r_t__term_8c.html#a09b5de01594f460479d09051e1171dea',1,'UART_Term_TxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a09b5de01594f460479d09051e1171dea',1,'UART_Term_TxCpltCallback(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
+  ['uart_5fterm_5fwait_5fready',['UART_Term_Wait_Ready',['../_u_a_r_t__term_8h.html#a5cd793a3d6d81b5bcb60a42b37f58a77',1,'UART_term.h']]]
 ];
--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -1,10 +1,5 @@
 var searchData=
 [
-  ['serial_5fdbg_5fflush_5frxbuf',['SERIAL_DBG_Flush_RxBuf',['../_u_a_r_t__term_8c.html#a86e0da1380675d097b7e9108f8ff23b5',1,'SERIAL_DBG_Flush_RxBuf(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a86e0da1380675d097b7e9108f8ff23b5',1,'SERIAL_DBG_Flush_RxBuf(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
-  ['serial_5fdbg_5flaunch_5fit_5frx',['SERIAL_DBG_Launch_It_Rx',['../_u_a_r_t__term_8c.html#a331a62475ab1e067c888b206784a7100',1,'SERIAL_DBG_Launch_It_Rx(UART_HandleTypeDef *huart):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a331a62475ab1e067c888b206784a7100',1,'SERIAL_DBG_Launch_It_Rx(UART_HandleTypeDef *huart):&#160;UART_term.c']]],
-  ['serial_5fdbg_5fmessage_5fhandler',['SERIAL_DBG_Message_Handler',['../_u_a_r_t__term_8c.html#a47d1b831af139df7723cb709ecb5c197',1,'SERIAL_DBG_Message_Handler(const char *msg, const uint8_t len):&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#a47d1b831af139df7723cb709ecb5c197',1,'SERIAL_DBG_Message_Handler(const char *msg, const uint8_t len):&#160;UART_term.c']]],
-  ['serial_5fdbg_5fsend',['SERIAL_DBG_Send',['../_u_a_r_t__term_8h.html#a2c48dfbcd2ee36a14394e17bd1e72686',1,'UART_term.h']]],
-  ['serial_5fdbg_5fwait_5fready',['SERIAL_DBG_Wait_Ready',['../_u_a_r_t__term_8h.html#a6cb9bf62236814f96f25a1a3027daae3',1,'UART_term.h']]],
   ['set_5fpwm_5fduty_5fbyte',['set_PWM_Duty_Byte',['../_p_w_m_8h.html#a512ec4a9e06ca804e8f34fcdcb50e331',1,'PWM.h']]],
   ['set_5fpwm_5fduty_5fperc',['set_PWM_Duty_Perc',['../_p_w_m_8h.html#a52a628ced46f7e2c4c02ce648ce027ed',1,'PWM.h']]],
   ['set_5fpwm_5fduty_5fscaled',['set_PWM_Duty_Scaled',['../_p_w_m_8c.html#a6862bdda2cde20ddee40f9183dea7dce',1,'set_PWM_Duty_Scaled(const TIM_HandleTypeDef *pTim, const uint32_t chan, const uint16_t duty, const uint16_t scale):&#160;PWM.c'],['../_p_w_m_8h.html#a6862bdda2cde20ddee40f9183dea7dce',1,'set_PWM_Duty_Scaled(const TIM_HandleTypeDef *pTim, const uint32_t chan, const uint16_t duty, const uint16_t scale):&#160;PWM.c']]],
