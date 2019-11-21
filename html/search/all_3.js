d704cd8eec717f4e21049f9ad40b1300a0b6077e
Travis CI <travis@travis-ci.org>
2019-11-21 18:27:12 +0000

    Deploy code docs to GitHub Pages Travis build: 36
    Commit: 763a5125efdc7ab468317eed096097af31306351

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -7,6 +7,7 @@ var searchData=
   ['dbg_5fmsg_5fout',['dbg_msg_out',['../stdream__rdir_8c.html#af60f4f11f4b14d02d0dc5564a238281d',1,'dbg_msg_out():&#160;stdream_rdir.c'],['../stdream__rdir_8h.html#af60f4f11f4b14d02d0dc5564a238281d',1,'dbg_msg_out():&#160;stdream_rdir.c']]],
   ['dbg_5fuart',['dbg_uart',['../_u_a_r_t__term_8c.html#ae4328d58bcdc00e56f77a78c1674e8af',1,'dbg_uart():&#160;UART_term.c'],['../_u_a_r_t__term_8h.html#ae4328d58bcdc00e56f77a78c1674e8af',1,'dbg_uart():&#160;UART_term.c']]],
   ['deinit_5ftim_5fbase',['deinit_TIM_Base',['../_p_w_m_8h.html#ab8dee9f45123fb0407ae75554df0721c',1,'PWM.h']]],
+  ['delay_5fus',['Delay_us',['../tick__utils_8c.html#af9dfe7ae2fdc14569aea2f3e22f85e99',1,'Delay_us(const uint32_t us):&#160;tick_utils.c'],['../tick__utils_8h.html#af9dfe7ae2fdc14569aea2f3e22f85e99',1,'Delay_us(const uint32_t us):&#160;tick_utils.c']]],
   ['diffdatetime',['diffDateTime',['../time__utils_8c.html#add92b553f649e9cf394cbbb73f23f907',1,'diffDateTime(const DateTime *const time2, const DateTime *const time1):&#160;time_utils.c'],['../time__utils_8h.html#add92b553f649e9cf394cbbb73f23f907',1,'diffDateTime(const DateTime *const time2, const DateTime *const time1):&#160;time_utils.c']]],
   ['duty',['duty',['../structlogic_p_w_m.html#a102275dc178a220cf46e96cabdbdc44f',1,'logicPWM']]]
 ];
